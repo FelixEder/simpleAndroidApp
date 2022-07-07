94fec528e3816c696475060eafc6b8d22fa2b489
Felix Eder <felix.eder@gmx.com>
2022-07-07 22:03:47 +0200

    First version of DetailsFragment

--- app/src/main/java/com/example/simpleandroidapp/details/DetailsFragment.kt
+++ app/src/main/java/com/example/simpleandroidapp/details/DetailsFragment.kt
@@ -1,4 +1,59 @@
 package com.example.simpleandroidapp.details
 
-class DetailsFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.simpleandroidapp.MainActivity
+import com.example.simpleandroidapp.R
+import com.example.simpleandroidapp.databinding.FragmentDetailsBinding
+
+class DetailsFragment: Fragment() {
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+    private val model: DetailsViewModel by viewModels()
+    private var args: Bundle? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater)
+
+        args = arguments
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindUsernameData()
+        setButtonClickListeners()
+        //setupObservers()
+    }
+
+    private fun setButtonClickListeners() {
+        binding.logoutButton.setOnClickListener {
+            (activity as MainActivity).goToLoginFragment()
+        }
+    }
+
+    private fun bindUsernameData() {
+        val username = args?.get((activity as MainActivity).USERNAME_KEY)
+        val welcomeText = getString(R.string.welcome, username)
+        binding.welcomeText.text = welcomeText
+    }
+
+    private fun setupObservers() {
+        TODO("Implement")
+    }
+
+    private fun bindGifData() {
+        TODO("Bind the gif data and metadata here")
+    }
+
 }
