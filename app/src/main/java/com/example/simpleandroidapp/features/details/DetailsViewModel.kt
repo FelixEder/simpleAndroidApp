7f9dd5b4de84d9e4e28c25a1fc045854d0f371a4
FelixEder <felix.eder@gmx.com>
2022-07-08 14:58:13 +0200

    Add data fetching to details view model

--- app/src/main/java/com/example/simpleandroidapp/features/details/DetailsViewModel.kt
+++ app/src/main/java/com/example/simpleandroidapp/features/details/DetailsViewModel.kt
@@ -1,7 +1,49 @@
-package com.example.simpleandroidapp.details
+package com.example.simpleandroidapp.features.details
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.simpleandroidapp.api.GiphyDataSource
+import com.example.simpleandroidapp.models.GiphyData
+import kotlinx.coroutines.launch
 
 class DetailsViewModel : ViewModel() {
+    private val giphyDataSource = GiphyDataSource()
 
+    private val _gifFetching = MutableLiveData<ViewState>()
+    val gifFetching: LiveData<ViewState> = _gifFetching
+
+    fun fetchData() {
+        _gifFetching.value = ViewState.Loading
+        viewModelScope.launch {
+            try {
+                val giphyDataResponse = giphyDataSource.requestTrendingGifs()
+
+                if (giphyDataResponse.isSuccess) {
+                    _gifFetching.value = ViewState.LoadingSuccessful(
+                        giphyDataResponse.getOrNull()
+                    )
+                } else {
+                    _gifFetching.value = ViewState.LoadingFailure(
+                        "Error loading gifs"
+                    )
+                }
+            } catch (e: Exception) {
+                _gifFetching.value = ViewState.LoadingFailure(
+                    "Error loading gifs"
+                )
+            }
+        }
+    }
+
+    sealed class ViewState {
+        object Loading: ViewState()
+        data class LoadingSuccessful(
+            val giphyData: GiphyData?
+        ) : ViewState()
+        data class LoadingFailure(
+            val errorMessage: String? = null
+        ) : ViewState()
+    }
 }
